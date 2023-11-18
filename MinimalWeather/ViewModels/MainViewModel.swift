55f89f56d77fc0843b073d4c79e4fd086ececced
Yegor Cheprasov <YegorCheprasov@gmail.com>
2023-11-18 18:32:01 +0300

    search_bar
    Create search bar

---
+++ MinimalWeather/ViewModels/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModel.swift
+//  MinimalWeather
+//
+//  Created by Yegor Cheprasov on 18.11.23.
+//
+
+import Foundation
+
+@MainActor
+class MainViewModel : ObservableObject {
+    
+}
---
+++ MinimalWeather/Views/SearchView.swift
@@ -0,0 +1,39 @@
+//
+//  SearchView.swift
+//  MinimalWeather
+//
+//  Created by Yegor Cheprasov on 18.11.23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @State
+    var searchText: String = ""
+    
+    var body: some View {
+        HStack {
+            TextField("Search city", text: $searchText)
+                .frame(width: .infinity)
+            if (searchText.isEmpty) {
+                Image(systemName: "magnifyingglass")
+                    .foregroundStyle(Color(red: 0.77, green: 0.77, blue: 0.77))
+            } else {
+                Image(systemName: "xmark")
+                    .foregroundStyle(Color(red: 0.77, green: 0.77, blue: 0.77))
+                    .onTapGesture {
+                        searchText.removeAll()
+                    }
+            }
+        }
+        .padding()
+        .frame(height: 46)
+        .background((RoundedRectangle(cornerRadius: 15).fill(Color(red: 0.99, green: 0.99, blue: 0.99))))
+        .padding()
+    }
+}
+
+#Preview {
+    SearchView()
+}
